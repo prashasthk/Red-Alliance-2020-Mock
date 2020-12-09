905e514893ad22fc1d77eb024f6142b4e410540c
Dave Melkani <davemelkani@gmail.com>
2020-12-09 14:14:17 -0800

    added drivetrain adding other mechanism
    added drivetrain adding other mechanism

---
+++ 2020RedAlliance/src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -0,0 +1,64 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+import frc.robot.RobotMap;
+import frc.robot.OI;
+import edu.wpi.first.wpilibj.PWMTalonSRX;
+
+/**
+ * Add your docs here.
+ */
+public class DriveTrain extends Subsystem {
+
+  private PWMTalonSRX left = new PWMTalonSRX(RobotMap.leftDrivePort);
+  private PWMTalonSRX right = new PWMTalonSRX(RobotMap.rightDrivePort);
+  private static DriveTrain drive;
+
+  private double leftPower, rightPower;
+
+  public DriveTrain() {
+    left.setInverted(false);
+    right.setInverted(true);
+  }
+  
+  public static DriveTrain getInstance() {
+    if(drive == null) {
+      drive = new DriveTrain();
+    }
+    return drive;
+  }
+
+  public void tankDrive(double leftPow, double rightPow) {
+    left.set(leftPow);
+    right.set(rightPow);
+  }
+
+  // Put methods for controlling this subsystem
+  // here. Call these from Commands.
+
+  @Override
+  public void initDefaultCommand() {}
+
+  @Override
+  public void periodic() {
+    
+    leftPower = OI.getArcJoy().getY();
+    rightPower = OI.getArcJoy().getY();
+    if(OI.getArcJoy().getX() < -0.2) {
+      leftPower += -0.5;
+      rightPower += 0.5;
+    } else if (OI.getArcJoy().getX() > 0.2) {
+      leftPower += 0.5;
+      rightPower += -0.5;
+    }
+
+    tankDrive(leftPower * 0.3, rightPower * 0.3);
+  }
+}
